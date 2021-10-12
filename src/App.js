61b7992df9c79d55a68619b9e6cf38f2463a1a92
WebdevAlex <twitchingalex@gmail.com>
2021-10-12 10:26:04 +0200

    Fix headline

--- src/App.js
+++ src/App.js
@@ -1,36 +1,39 @@
-import './App.css';
-import ImageUploader from './components/ImageUploader';
-import ImageViewer from './components/ImageView';
-import TileSettings from './components/TileSettings';
-import useStore from './store';
+import "./App.css";
+import ImageUploader from "./components/ImageUploader";
+import ImageViewer from "./components/ImageView";
+import TileSettings from "./components/TileSettings";
+import useStore from "./store";
 
 function App() {
   const { image, menuCollapsed } = useStore();
 
   return (
     <div className="App">
-      <nav className="navbar is-dark is-fixed-top" role="navigation" aria-label="main navigation">
+      <nav
+        className="navbar is-dark is-fixed-top"
+        role="navigation"
+        aria-label="main navigation"
+      >
         <div className="container">
-          <div className="navbar-item has-text-light">
-            Tileset Grid Indexer
-          </div>
+          <div className="navbar-item has-text-light">Tilemap Indexer</div>
         </div>
       </nav>
       <div className="container has-padding-top-20 pt-5 pb-5">
         <div className="columns">
-          {image &&
+          {image && (
             <>
               <div className="column">
                 <ImageViewer />
               </div>
-              {!menuCollapsed && <div className="column is-one-fifth">
-                <TileSettings />
-              </div>}
-            </>}
-
+              {!menuCollapsed && (
+                <div className="column is-one-fifth">
+                  <TileSettings />
+                </div>
+              )}
+            </>
+          )}
         </div>
         {!image && <ImageUploader />}
-
       </div>
     </div>
   );
